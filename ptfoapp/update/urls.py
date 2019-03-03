e0a5a3021c3727cbbdd5f725eb43b609090df3ff
Dhivya Senthil Murugan <senthilm@usc.edu>
2019-03-03 02:42:47 -0800

    slightly more functions

--- ptfoapp/update/urls.py
+++ ptfoapp/update/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:printer_id>/', views.status, name = "status"),
+    path('<int:printer_id>/update/', views.newUpdate, name = "newUpdate")
 ]
